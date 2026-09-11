c6fcf4c8ea5504eea7e65432105d9d950d422767
mushee <musheelabs@proton.me>
2026-09-11 13:49:59 +0100

    feat(routes): add disclosures workspace

---
+++ src/app/disclosures/page.tsx
@@ -0,0 +1,5 @@
+import { Milestones712ClientOnly } from "@/components/Milestones712ClientOnly";
+
+export default function DisclosuresPage() {
+  return <Milestones712ClientOnly section="disclosures" />;
+}
