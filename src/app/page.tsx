6a0b067b1309e6223c93ac702c7148cf233108bc
mushee <musheelabs@proton.me>
2026-09-11 13:48:04 +0100

    refactor(routes): make root a dedicated dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,5 @@
 import { BlackpayAppClientOnly } from "@/components/BlackpayAppClientOnly";
-import { PreviewRuntimeClientOnly } from "@/components/PreviewRuntimeClientOnly";
-import { Milestones712ClientOnly } from "@/components/Milestones712ClientOnly";
 
 export default function HomePage() {
-  return (
-    <>
-      <BlackpayAppClientOnly />
-      <section className="shell liveShell">
-        <PreviewRuntimeClientOnly />
-      </section>
-      <Milestones712ClientOnly />
-    </>
-  );
+  return <BlackpayAppClientOnly section="dashboard" />;
 }
